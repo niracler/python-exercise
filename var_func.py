978bb49c0915efdc8e44fbe5ff4747170bf84821
niracler <1026037967@qq.com>
2018-01-25 09:46:45 +0800

    函数参数的练习

---
+++ var_func.py
@@ -0,0 +1,30 @@
+# 接收一个tuple,这是一件多么奇妙的事情。
+def product(*args):
+    if(args == ()):
+        raise TypeError
+    else:
+        sum = 1
+        for i in args:
+            sum *= i
+        return sum
+
+
+# 测试
+print('product(5) =', product(5))
+print('product(5, 6) =', product(5, 6))
+print('product(5, 6, 7) =', product(5, 6, 7))
+print('product(5, 6, 7, 9) =', product(5, 6, 7, 9))
+if product(5) != 5:
+    print('测试失败!')
+elif product(5, 6) != 30:
+    print('测试失败!')
+elif product(5, 6, 7) != 210:
+    print('测试失败!')
+elif product(5, 6, 7, 9) != 1890:
+    print('测试失败!')
+else:
+    try:
+        product()
+        print('测试失败!')
+    except TypeError:
+        print('测试成功!')
