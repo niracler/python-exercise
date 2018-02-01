09ff0eb27ffc3912f3d85e0f1e4f4b8ca8d062e7
niracler <1026037967@qq.com>
2018-02-01 08:50:07 +0800

    面向对象的编程练习

---
+++ oop/protected_student.py
@@ -0,0 +1,26 @@
+# 请把下面的Student对象的gender字段对外隐藏起来，用get_gender()和set_gender()代替，并检查参数有效性：
+
+# -*- coding: utf-8 -*-
+
+class Student(object):
+    def __init__(self, name, gender):
+        self.name = name
+        self.__gender = gender
+
+    def get_gender(self):
+        return self.__gender
+
+    def set_gender(self, gender):
+        self.__gender = gender
+
+
+# 测试:
+bart = Student('Bart', 'male')
+if bart.get_gender() != 'male':
+    print('测试失败!')
+else:
+    bart.set_gender('female')
+    if bart.get_gender() != 'female':
+        print('测试失败!')
+    else:
+        print('测试成功!')
