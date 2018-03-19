8e5f0bcfdfc9da80493e32aac71249227260c88c
niracler <1026037967@qq.com>
2018-03-19 09:11:14 +0800

    老司机爬虫，下次试一下并发以及爬取磁力链接

--- reptile/old_driver/do_driver_all.py
+++ reptile/old_driver/do_driver_all.py
@@ -4,7 +4,7 @@ import time
 
 from reptile.old_driver.do_requests import driver
 
-for i in range(96, 236):
+for i in range(1, 500):
     url = "https://www.llss.fun/wp/page/" + str(i)
     filename = "info/"+"page" + str(i) + ".json"
     # threading.Thread(target=driver, args=(url, filename,)).start()
