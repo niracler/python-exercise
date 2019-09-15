92c44fd47680b6ae726a92734881af5e4c1adabb
Niracler <1026037967@qq.com>
2019-09-15 21:14:37 +0800

    Implement the following operations of a queue using stacks.

--- leetcode/stack_queue/implement_queue_using_stacks.py
+++ leetcode/stack_queue/implement_queue_using_stacks.py
@@ -16,9 +16,13 @@
 # queue.empty(); // returns false
 # Notes:
 #
-# You must use only standard operations of a stack -- which means only push to top, peek/pop from top, size, and is empty operations are valid.
-# Depending on your language, stack may not be supported natively. You may simulate a stack by using a list or deque (double-ended queue), as long as you use only standard operations of a stack.
-# You may assume that all operations are valid (for example, no pop or peek operations will be called on an empty queue).
+# You must use only standard operations of a stack
+# -- which means only push to top, peek/pop from top, size, and is empty operations are valid.
+# Depending on your language, stack may not be supported natively.
+# You may simulate a stack by using a list or deque (double-ended queue),
+# as long as you use only standard operations of a stack.
+# You may assume that all operations are valid
+# (for example, no pop or peek operations will be called on an empty queue).
 #
 # url:https://leetcode.com/problems/implement-queue-using-stacks/
 # """
@@ -30,26 +34,39 @@ class MyQueue:
         """
         Initialize your data structure here.
         """
+        self.s1 = []
+        self.s2 = []
 
     def push(self, x: int) -> None:
         """
         Push element x to the back of queue.
         """
+        self.s1.append(x)
 
     def pop(self) -> int:
         """
         Removes the element from in front of queue and returns that element.
         """
 
+        self.peek()
+
+        return self.s2.pop()
+
     def peek(self) -> int:
         """
         Get the front element.
         """
+        if not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())  # 弹出s1中的最上一个到s2里面
+
+        return self.s2[-1]
 
     def empty(self) -> bool:
         """
         Returns whether the queue is empty.
         """
+        return not self.s1 and not self.s2
 
 
 # Your MyQueue object will be instantiated and called as such:
@@ -61,10 +78,15 @@ class MyQueue:
 
 
 if __name__ == '__main__':
-    a = ""
-
+    queue = MyQueue()
+    queue.push(1)
+    queue.push(2)
+    print(queue.peek())  # returns 1
+    print(queue.pop())  # returns 1
+    print(queue.empty())  # returns false
 
 # """
-# 分析:
+# 分析: 话说，这道题能用Python做么？
+# 进的时候从s1进,出的时候从s2出,要是s2空了，就将s1的东西弹出来，装进去
 #
 # """
