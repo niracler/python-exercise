2078d9bc564956bc28c827089d2532eb548138d6
Niracler <1026037967@qq.com>
2019-09-23 21:04:16 +0800

    wait

---
+++ leetcode/hash_table/three_sum_closest.py
@@ -0,0 +1,33 @@
+# """
+# 题目：Given an array nums of n integers and an integer target,
+# find three integers in nums such that the sum is closest to target.
+# Return the sum of the three integers.
+# You may assume that each input would have exactly one solution.
+#
+# Example:
+#
+# Given array nums = [-1, 2, 1, -4], and target = 1.
+#
+# The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+#
+# url:https://leetcode.com/problems/3sum-closest/
+# """
+from typing import List
+
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        return 5
+
+
+if __name__ == '__main__':
+    a = []
+
+    solution = Solution()
+    result = solution.threeSumClosest(a,5)
+    print(result)
+
+# """
+# 分析:
+#
+# """
